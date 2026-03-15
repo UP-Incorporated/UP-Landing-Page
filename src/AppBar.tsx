a887da8764b14521783438d5b7a0c763d741f7e8
Henry S. <henrypaulschaar@gmail.com>
2026-03-15 18:20:16 +0100

    Make AppBar take Children Components

--- src/AppBar.tsx
+++ src/AppBar.tsx
@@ -1,11 +1,14 @@
 import { Component } from "react";
-
-export class AppBar extends Component {
+import type { ReactNode } from "react";
+interface AppBarProps {
+    children: ReactNode;
+}
+export class AppBar extends Component<AppBarProps> {
     render() {
         return (
             <>
                 <header className="bg-stone-800 border-b border-b-stone-600 h-16">
-
+                    {this.props.children}
                 </header>
             </>
         )
